e3c9a7a7428b30b236392dcb2b2b3fafa3e7d894
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-08-08 12:40:00 +0200

    Add waiting spinner to register and login screens

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 
 // Component imports
 import 'package:flash_chat/components/rounded_button.dart';
@@ -27,70 +28,77 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
+      body: ProgressHUD(
+        child: Builder(
+          builder: (context) => Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 48.0,
+                ),
+                // Email text field
+                TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your email.',
+                    )
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                // Password text field
+                TextField(
+                    obscureText: true,
+                    textAlign: TextAlign.center,
+                    onChanged: (value) {
+                      password = value;
+                    },
+                    decoration: kTextFieldDecoration.copyWith(
+                      hintText: 'Enter your password.',
+                    )
+                ),
+                const SizedBox(
+                  height: 24.0,
+                ),
+                // Login button
+                RoundedButton(
+                    colour: Colors.lightBlueAccent,
+                    title: 'Log In',
+                    onPressed: () async {
+                      final progress = ProgressHUD.of(context);
+                      progress!.showWithText('Loading...');
+
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                            email: email, password: password);
+                        if (user != null) {
+                          Navigator.pushNamed(context, ChatScreen.id);
+                        }
+                        progress.dismiss();
+                      } catch (e) {
+                        print(e);
+                        progress.dismiss();
+                      }
+                    }
+                )
+              ],
             ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            // Email text field
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email.',
-              )
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            // Password text field
-            TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password.',
-              )
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            // Login button
-            RoundedButton(
-              colour: Colors.lightBlueAccent,
-              title: 'Log In',
-              onPressed: () async {
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  if (user != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  }
-                } catch (e) {
-                  print('Error: $e');
-                }
-              }
-            )
-          ],
-        ),
+          ),
+        )
       ),
     );
   }
